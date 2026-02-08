9683b565f802a28f5d54cbe7be582cf2d4decdd5
Diego <diegoneves2021123@gmail.com>
2026-02-08 00:26:56 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/Ecstasy-Fivem/',
   build: {
     outDir: 'dist'
   },
-  base: '/',
   server: {
     port: 5173,
     open: true
