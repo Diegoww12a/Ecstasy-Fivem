44199639022852685a49c0a77c42b57294125f65
Diego <diegoneves2021123@gmail.com>
2026-02-08 00:49:36 -0300

    rename video file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Ecstasy-Fivem/',
-  build: {
-    outDir: 'dist'
+  base: '/Cheat-stopped-2.0/',
+  server: {
+    port: 5173,
+    open: true
   }
 })
