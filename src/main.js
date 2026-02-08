e26695a07961eb0580e7f87794efc5a4622a67ba
Diego <diegoneves2021123@gmail.com>
2026-02-08 00:19:43 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import './style.css'
+
+
+
+const botao = document.getElementById("botao");
+const side = document.getElementById("side");
+
+botao.addEventListener("click", () => {
+    side.classList.toggle("hidden")
+    
+})
+
+const video = document.getElementById("video");
+
+video.src = "/cheat.mp4";
+video.autoplay = true;
+video.muted = true;      // obrigatório pra autoplay
+video.loop = true;
+video.playsInline = true;
+video.controls = false; // tira tudo
+
+
+const play = document.getElementById("play")
+
+play.src = "/video loop.webm"
+play.autoplay = true;
+play.muted = true;
+play.loop = true;
+play.playsInline = true;
+play.controls = false;
+
+
+
+document.querySelectorAll(".faq-btn").forEach(bt => {
+    bt.addEventListener("click", () => {
+        const conteudo = bt.nextElementSibling;
+        conteudo.classList.toggle("max-h-40")
+        conteudo.classList.toggle("max-h-0");
+        const icon = bt.querySelector("span");
+        icon.classList.toggle("rotate-45")
+    })
+})
