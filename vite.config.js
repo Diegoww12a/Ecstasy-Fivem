cd7e4af3d615cc0f96e865ae91b9be0c3360cbc9
Diego <diegoneves2021123@gmail.com>
2026-02-08 00:54:53 -0300

    teste usar nos 2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github'
+    ? '/Cheat-stopped-2.0/'
+    : '/',
   server: {
     port: 5173,
     open: true
   }
-})
+}))
